0ba317bb93292a501ed70b456498c96b0a980987
Bae Jin Woo <jw969696@naver.com>
2020-08-04 15:10:12 +0900

    a

--- naver_movies.py
+++ naver_movies.py
@@ -1,22 +1,26 @@
 import requests
+import json
+from collections import OrderedDict
 from bs4 import BeautifulSoup
-
+file_data = OrderedDict()
 raw = requests.get("https://movie.naver.com/movie/running/current.nhn#")
 
 html = BeautifulSoup(raw.text, 'html.parser')
 
-# f = open("movie.csv", 'w')
-# f.write('제목', '코드\n')
-
-movie_list = {}
+movie_list = []
 
 movies = html.select('ul.lst_detail_t1 dl.lst_dsc')
 
 for movie in movies:
     title = movie.select_one('dt.tit a').text
     code = movie.select_one('dt.tit a')['href'].split("code=="[-1])
-    print(title)
-    print(code[-1])
-    movie_list['title'] = title
-    movie_list['code'] = code
-print(movie_list)
+    # print(title)
+    # print(code[-1])
+    movie_dic = {}
+    movie_dic['title'] = title
+    movie_dic['code'] = code[-1]
+    movie_list.append(movie_dic)
+# print(movie_list)
+
+with open('movie.json', 'w', encoding='utf-8') as f:
+    json.dump(movie_list, f, ensure_ascii=False, indent=4)
